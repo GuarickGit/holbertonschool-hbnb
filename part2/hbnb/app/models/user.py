eac6431641a13f93d866f08e14fef149b0e331dd
GuarickGit <10615@holbertonstudents.com>
2025-06-21 11:00:44 +0200

    Update | Add not isinstance for str

--- part2/hbnb/app/models/user.py
+++ part2/hbnb/app/models/user.py
@@ -5,11 +5,11 @@ class User(BaseModel):
     def __init__(self, first_name, last_name, email, is_admin=False):
         super().__init__()
 
-        if not first_name or len(first_name) > 50:
+        if not isinstance(first_name, str) or not first_name or len(first_name) > 50:
             raise ValueError("Invalid first_name")
-        if not last_name or len(last_name) > 50:
+        if not isinstance(last_name, str) or not last_name or len(last_name) > 50:
             raise ValueError("Invalid last_name")
-        if not email or not is_valid_email(email):
+        if not isinstance(email, str) or not email or not is_valid_email(email):
             raise ValueError("Invalid email")
 
         self.first_name = first_name
@@ -19,17 +19,17 @@ class User(BaseModel):
 
     def update(self, data):
         if "first_name" in data:
-            if not data["first_name"] or len(data["first_name"]) > 50:
+            if not isinstance(data["first_name"], str) or not data["first_name"] or len(data["first_name"]) > 50:
                 raise ValueError("Invalid first_name")
             self.first_name = data["first_name"]
 
         if "last_name" in data:
-            if not data["last_name"] or len(data["last_name"]) > 50:
+            if not isinstance(data["last_name"], str) or not data["last_name"] or len(data["last_name"]) > 50:
                 raise ValueError("Invalid last_name")
             self.last_name = data["last_name"]
 
         if "email" in data:
-            if not data["email"] or not is_valid_email(data["email"]):
+            if not isinstance(data["email"], str) or not data["email"] or not is_valid_email(data["email"]):
                 raise ValueError("Invalid email")
             self.email = data["email"]
 
