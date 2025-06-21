26280e8cf2bd2962898fa0c036f7a57a478eba7d
GuarickGit <10615@holbertonstudents.com>
2025-06-21 11:00:00 +0200

    Update | Add update method

--- part2/hbnb/app/models/amenity.py
+++ part2/hbnb/app/models/amenity.py
@@ -1,10 +1,19 @@
 from app.models.base_model import BaseModel
 
+
 class Amenity(BaseModel):
     def __init__(self, name):
         super().__init__()
 
-        if not name or len(name) > 50:
+        if not isinstance(name,str) or not name or len(name) > 50:
             raise ValueError("Invalid amenity name")
 
         self.name = name
+
+    def update(self, data):
+        if "name" in data:
+            if not isinstance(data["name"], str) or not data["name"] or len(data["name"]) > 50:
+                raise ValueError("Invalid name")
+            self.name = data["name"]
+
+        self.save()  # met à jour updated_at
