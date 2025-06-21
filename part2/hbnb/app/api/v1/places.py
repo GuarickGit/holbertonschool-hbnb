eb08ffeab3c53443bae8873f960827aa06907f4e
GuarickGit <10615@holbertonstudents.com>
2025-06-21 22:37:42 +0200

    Update | Add review model and update place model

--- part2/hbnb/app/api/v1/places.py
+++ part2/hbnb/app/api/v1/places.py
@@ -16,8 +16,15 @@ user_model = api.model('PlaceUser', {
     'email': fields.String(description='Email of the owner')
 })
 
-# Define the place model for input validation and documentation
-place_model = api.model('Place', {
+review_model = api.model('PlaceReview', {
+    'id': fields.String(description='Review ID'),
+    'text': fields.String(description='Text of the review'),
+    'rating': fields.Integer(description='Rating of the place (1-5)'),
+    'user_id': fields.String(description='ID of the user')
+})
+
+# Model utilisé uniquement pour les POST / PUT
+place_input_model = api.model('PlaceInput', {
     'title': fields.String(required=True, description='Title of the place'),
     'description': fields.String(description='Description of the place'),
     'price': fields.Float(required=True, description='Price per night'),
@@ -28,9 +35,22 @@ place_model = api.model('Place', {
 })
 
 
+place_model = api.model('Place', {
+    'title': fields.String(required=True, description='Title of the place'),
+    'description': fields.String(description='Description of the place'),
+    'price': fields.Float(required=True, description='Price per night'),
+    'latitude': fields.Float(required=True, description='Latitude of the place'),
+    'longitude': fields.Float(required=True, description='Longitude of the place'),
+    'owner_id': fields.String(required=True, description='ID of the owner'),
+    'owner': fields.Nested(user_model, description='Owner of the place'),
+    'amenities': fields.List(fields.String, required=True, description="List of amenities ID's"),
+    'reviews': fields.List(fields.Nested(review_model), description='List of reviews')
+})
+
+
 @api.route('/')
 class PlaceList(Resource):
-    @api.expect(place_model)
+    @api.expect(place_input_model, validate=True)
     @api.response(201, 'Place successfully created')
     @api.response(400, 'Invalid input data')
     def post(self):
@@ -91,10 +111,18 @@ class PlaceResource(Resource):
             },
             'amenities': [
                 {'id': a.id, 'name': a.name} for a in place.amenities
+            ],
+            'reviews': [
+                {
+                    'id': r.id,
+                    'text': r.text,
+                    'rating': r.rating,
+                    'user_id': r.user.id
+                } for r in place.reviews
             ]
         }, 200
 
-    @api.expect(place_model)
+    @api.expect(place_input_model, validate=True)
     @api.response(200, 'Place updated successfully')
     @api.response(404, 'Place not found')
     @api.response(400, 'Invalid input data')
