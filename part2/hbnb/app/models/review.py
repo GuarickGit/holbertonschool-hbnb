9f83cdede67df8e47b80ff8594207035aa0543e3
GuarickGit <10615@holbertonstudents.com>
2025-06-21 22:38:11 +0200

    Update | add update method

--- part2/hbnb/app/models/review.py
+++ part2/hbnb/app/models/review.py
@@ -1,15 +1,26 @@
 from app.models.base_model import BaseModel
 
 class Review(BaseModel):
-    def __init__(self, text, rating, place, user):
+    def __init__(self, text, rating, user, place):
         super().__init__()
-
-        if not text:
-            raise ValueError("Review text is required")
-        if not (1 <= rating <= 5):
-            raise ValueError("Rating must be between 1 and 5")
-
+        if not text or not isinstance(text, str):
+            raise ValueError("Invalid review text")
+        if not isinstance(rating, int) or not (1 <= rating <= 5):
+            raise ValueError("Rating must be an integer between 1 and 5")
         self.text = text
-        self.rating = int(rating)
+        self.rating = rating
+        self.user = user  # instance de User
         self.place = place  # instance de Place
-        self.user = user    # instance de User
+
+    def update(self, review_data):
+        if "text" in review_data:
+            if not review_data["text"] or not isinstance(review_data["text"], str):
+                raise ValueError("Invalid review text")
+            self.text = review_data["text"]
+
+        if "rating" in review_data:
+            if not isinstance(review_data["rating"], int) or not (1 <= review_data["rating"] <= 5):
+                raise ValueError("Invalid rating")
+            self.rating = review_data["rating"]
+
+        self.save()
