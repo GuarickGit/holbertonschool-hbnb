892de0883bfbec726e8e9eb1bb02cd035ee75e36
GuarickGit <10615@holbertonstudents.com>
2025-06-30 15:57:26 +0200

    Update | add password to user_model and modification of the return in POST

--- part3/hbnb/app/api/v1/users.py
+++ part3/hbnb/app/api/v1/users.py
@@ -7,7 +7,8 @@ api = Namespace('users', description='User operations')
 user_model = api.model('User', {
     'first_name': fields.String(required=True, description='First name of the user'),
     'last_name': fields.String(required=True, description='Last name of the user'),
-    'email': fields.String(required=True, description='Email of the user')
+    'email': fields.String(required=True, description='Email of the user'),
+    'password': fields.string(required=True, description='Password of the user')
 })
 
 
@@ -36,9 +37,7 @@ class UserList(Resource):
 
         return {
             'id': new_user.id,
-            'first_name': new_user.first_name,
-            'last_name': new_user.last_name,
-            'email': new_user.email
+            'message': 'User successfully created'
         }, 201
 
     @api.response(200, 'List of users retrieved successfully')
