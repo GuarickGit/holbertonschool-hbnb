35463adfab32a91bbb97f1628d21da2020b5a2b1
GuarickGit <10615@holbertonstudents.com>
2025-07-03 15:52:34 +0200

    Update | Transition from in-memory persistence to a database-backed persistence

--- part3/hbnb/app/services/facade.py
+++ part3/hbnb/app/services/facade.py
@@ -1,4 +1,4 @@
-from app.persistence.repository import InMemoryRepository
+from app.persistence.repository import SQLAlchemyRepository
 from app.models.user import User
 from app.models.amenity import Amenity
 from app.models.place import Place
@@ -15,10 +15,10 @@ class HBnBFacade:
         """
         Initializes repositories for users, places, reviews, and amenities.
         """
-        self.user_repo = InMemoryRepository()
-        self.place_repo = InMemoryRepository()
-        self.review_repo = InMemoryRepository()
-        self.amenity_repo = InMemoryRepository()
+        self.user_repo = SQLAlchemyRepository(User)
+        self.place_repo = SQLAlchemyRepository(Place)
+        self.review_repo = SQLAlchemyRepository(Review)
+        self.amenity_repo = SQLAlchemyRepository(Amenity)
 
     # -------------------------------
     # User Methods
