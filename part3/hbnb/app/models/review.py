e77f48961dceb3a0aaa01be3974effea52d873b2
GuarickGit <10615@holbertonstudents.com>
2025-07-09 15:43:37 +0200

    Update | Remove the 1 from Integer - line 7

--- part3/hbnb/app/models/review.py
+++ part3/hbnb/app/models/review.py
@@ -4,7 +4,7 @@ from app.extensions import db
 class Review(BaseModel):
     __tablename__= 'reviews'
     text = db.Column(db.String(100), nullable=False)
-    rating = db.Column(db.Integer(1), nullable=False)
+    rating = db.Column(db.Integer, nullable=False)
 
     def update(self, review_data):
         """
