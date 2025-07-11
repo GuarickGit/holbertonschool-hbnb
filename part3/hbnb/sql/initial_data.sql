936a7f68c067a4fba70c99257803634210b0708b
GuarickGit <10615@holbertonstudents.com>
2025-07-11 12:42:20 +0200

    Add initial_data.sql

---
+++ part3/hbnb/sql/initial_data.sql
@@ -0,0 +1,11 @@
+INSERT INTO users (id, email, first_name, last_name, password, is_admin)
+VALUES ('36c9050e-ddd3-4c3b-9731-9f487208bbc1', 'admin@hbnb.io', 'Admin', 'HBnB', '$2b$12$rYAeW1nlTrvgo9XW6gSuU.zJ5QZrFt6R/KybMB94brmdyLxgQwODm', TRUE);
+
+INSERT INTO amenities (id, name)
+VALUES ('fb72e203-7092-4a9b-a33d-6e3e572d17be', 'WiFi');
+
+INSERT INTO amenities (id, name)
+VALUES ('590ae12b-a8ab-490a-aea7-c161ec5f99d8', 'Swimming Pool');
+
+INSERT INTO amenities (id, name)
+VALUES ('630920e5-5a12-4abf-94b0-75384cb8b7d9', 'Air Conditioning');
