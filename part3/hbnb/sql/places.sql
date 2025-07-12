d5dff9f7a6c6ef80402c8de8ed645feb4bf8d500
GuarickGit <10615@holbertonstudents.com>
2025-07-12 20:52:06 +0200

    Update | Add NOT NULL to description

--- part3/hbnb/sql/places.sql
+++ part3/hbnb/sql/places.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS places(
 	id CHAR(36) PRIMARY KEY,
 	title VARCHAR(255) NOT NULL,
-	description TEXT,
+	description TEXT NOT NULL,
 	price DECIMAL(10, 2) NOT NULL,
 	latitude FLOAT NOT NULL,
 	longitude FLOAT NOT NULL,
